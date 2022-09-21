b97bb47ad59de4e43f9652b975207fda88b7812b
jmogmat <jmog.matias@icloud.com>
2022-09-22 00:01:33 +0200

    ArregloCarritoCompra

---
+++ api/deleteItem.php
@@ -0,0 +1,67 @@
+<?php
+
+session_start();
+
+require_once(__DIR__ . '/../autoload.php');
+
+use \functions\functions as func;
+use \conectDB\conectDB as conect;
+
+$tool = new func();
+
+$tool->checkSession();
+
+header('Content-type: application/json; charset=utf-8');
+
+if (isset($_SESSION['carrito'])) {
+
+    $db = new conect($_SESSION['rol']);
+
+    $itemId = $_POST['itemId'];
+
+    foreach ($_SESSION['carrito'] as $key => $value) {
+
+        if ($value['id'] === $itemId) {
+
+            if ($value['cantidad'] == 1) {
+
+                $indice = $key;
+
+                unset($_SESSION['carrito'][$key]);
+
+                $newArrayCart = [];
+
+                foreach ($_SESSION['carrito'] as $key => $value) {
+
+                    array_push($newArrayCart, $_SESSION['carrito'][$key]);
+                }
+
+                unset($_SESSION['carrito']);
+
+                if (!isset($_SESSION['carrito'])) {
+
+                    foreach ($newArrayCart as $item) {
+
+                        $_SESSION['carrito'][] = $item;
+                    
+                    }
+                    
+                     echo json_encode(['success' => '202']);
+                        return;
+                }
+            }
+
+            if ($value['cantidad'] > '1') {
+
+                $totalCantidad = $value['cantidad'] - 1;
+
+                $_SESSION['carrito'][$key] = array('id' => $value['id'], 'nombre' => $value['nombre'], 'descripcion' => $value['descripcion'], 'precio' => $value['precio'], 'cantidad' => $totalCantidad);
+
+                echo json_encode(['success' => '202']);
+                return;
+            }
+        }
+    }
+}
+?>
+
--- modalCart.php
+++ modalCart.php
@@ -1,204 +1,273 @@
-<div class="modal fade" id="modalcart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Mi carrito</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="modal-body">
-                    <div>
-                        <div class="p-2">
-                            <ul class="list-group mb-3">
-                                <?php
-                                if (isset($_SESSION['carrito'])) {
-                                    $total = 0;
-                                    $myCart = $_SESSION['carrito'];
-                                    echo "<pre>";
-                                    var_dump($myCart);
-                                    echo "</pre>";
-
-                                    for ($i = 0; $i <= count($myCart) - 1; $i++) {
-                                        if (isset($myCart[$i])) {
-                                            if ($myCart[$i] != NULL) {
-                                                ?>
-                                                <li class="list-group-item d-flex justify-content-between lh-condensed">
-                                                    <div class="row col-12" >
-                                                        <div class="col-6 p-0" style="text-align: left; color: #000000;"><h6 class="my-0">Cantidad: 
-                                                            <?php 
-                                                            
-                                                            echo $myCart[$i]['cantidad'] ?> : <?php echo $myCart[$i]['nombre']; ?></h6>
-                                                        </div>
-                                                        <div class="col-6 p-0"  style="text-align: right; color: #000000;" >
-                                                            <span class="text-muted" style="text-align: right; color: #000000;"><?php echo $myCart[$i]['precio']?> €</span>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                                <?php
-                                                $total = $total + ($myCart[$i]['precio'] * $myCart[$i]['cantidad']);
-                                            }
-                                        }
-                                    }
-                                }
-                                ?>
-                                <li class="list-group-item d-flex justify-content-between">
-                                    <span  style="text-align: left; color: #000000;">Total (EUR)</span>
-                                    <strong  id="total"  style="text-align: left; color: #000000;"><?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="modal fade" id="modalcart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Mi carrito</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="modal-body">
+                            <div>
+                                <div class="p-2">
+                                    <ul class="list-group mb-3">
+                                        <?php
                                         if (isset($_SESSION['carrito'])) {
                                             $total = 0;
+                                            $myCart = $_SESSION['carrito'];
+                                            /* echo "<pre>";
+                                              var_dump($myCart);
+                                              echo "</pre>"; */
+
                                             for ($i = 0; $i <= count($myCart) - 1; $i++) {
                                                 if (isset($myCart[$i])) {
                                                     if ($myCart[$i] != NULL) {
+                                                        ?>
+                                                        <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                                            <div class="row col-12" >
+                                                                <div class="col-6 p-0" style="text-align: left; color: #000000;">
+                                                                    <h6 class="my-0"><?php echo $myCart[$i]['nombre']; ?>
+
+                                                                        : <?php echo " x" . $myCart[$i]['cantidad']; ?>
+                                                                    </h6>
+                                                                </div>
+                                                                <div class="col-5 p-0"  style="text-align: right; color: #000000;">
+                                                                    <span class="text-muted" style="text-align: right; color: #000000;"><?php echo $myCart[$i]['precio'] ?> €</span>
+                                                                </div>
+                                                            </div>
+                                                            <button style="background-color: white; border: none" onclick="event.preventDefault();deleteItem(<?php echo $myCart[$i]['id'] ?>)"><i class="bi bi-trash3-fill" style="color: red;"></i></button>
+                                                        </li>
+                                                        <?php
                                                         $total = $total + ($myCart[$i]['precio'] * $myCart[$i]['cantidad']);
                                                     }
                                                 }
                                             }
                                         }
-                                        if (!isset($total)) {
-                                            $total = '0';
-                                        } else {
-                                            $total = $total;
-                                        }
-                                        echo $total;
-                                        ?> €</strong>
-                                </li>
-                            </ul>
+                                        ?>
+                                        <li class="list-group-item d-flex justify-content-between">
+                                            <span  style="text-align: left; color: #000000;">Total (EUR)</span>
+                                            <strong  id="total"  style="text-align: left; color: #000000;"><?php
+                                                if (isset($_SESSION['carrito'])) {
+                                                    $total = 0;
+                                                    for ($i = 0; $i <= count($myCart) - 1; $i++) {
+                                                        if (isset($myCart[$i])) {
+                                                            if ($myCart[$i] != NULL) {
+                                                                $total = $total + ($myCart[$i]['precio'] * $myCart[$i]['cantidad']);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                                if (!isset($total)) {
+                                                    $total = '0';
+                                                } else {
+                                                    $total = $total;
+                                                }
+                                                echo $total;
+                                                ?> €</strong>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <a type="button" class="btn btn-primary" href="" onclick="event.preventDefault();deleteCart()">Vaciar carrito</a>
+                        <a type="button" class="btn btn-success" href="" onclick="event.preventDefault();purchase()">Finalizar pedido</a>
                     </div>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <a type="button" class="btn btn-primary" href="" onclick="event.preventDefault();deleteCart()">Vaciar carrito</a>
-                <a type="button" class="btn btn-success" href="" onclick="event.preventDefault();purchase()">Finalizar pedido</a>
-            </div>
         </div>
-    </div>
-    <script>
 
-        var total = document.getElementById('total').innerText;
+        <script>
 
-
-        function purchase() {
+            var total = document.getElementById('total').innerText;
 
 
-            if (total !== '0 €') {
-
-                fetch('api/purchase.php', {
-                    method: 'POST',                   
+            function purchase() {
 
 
-                }).then(response => response.json())
-                        .then(data => {
+                if (total !== '0 €') {
 
-                            if (data.success) {
-                                Swal.fire({
-                                    icon: 'success',
-                                    title: 'Pedido confirmado',
-                                    text: 'Su pedido se ha realizado con éxito!',
-                                    confirmButtonText:
-                                            'Continuar',
+                    fetch('api/purchase.php', {
+                        method: 'POST',
 
-                                }).then((result) => {
+                    }).then(response => response.json())
+                            .then(data => {
 
-                                    if (result.isConfirmed) {
-                                       document.location.reload();
-                                       
-                                          
-                                    }
-                                })
-                                
-                                return;
-                            }
-                            if (data.error && data.msg) {
+                                if (data.success) {
+                                    Swal.fire({
+                                        icon: 'success',
+                                        title: 'Pedido confirmado',
+                                        text: 'Su pedido se ha realizado con éxito!',
+                                        confirmButtonText:
+                                                'Continuar',
 
-                                Swal.fire({
-                                    icon: 'error',
-                                    title: 'Ups...',
-                                    text: data.msg
-                                })
+                                    }).then((result) => {
 
-                            }
-                        })
-
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Ups...',
-                    text: 'El carrito está vacio!',
-                })
-            }
-        }
-
-        function deleteCart() {
+                                        if (result.isConfirmed) {
+                                            document.location.reload();
 
 
-            if (total !== '0 €') {
+                                        }
+                                    })
 
-                Swal.fire({
-                    title: 'Estás seguro/a?',
-                    text: "Se borrarán todos los productos del carrito!",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Si, vaciar carrito!',
-                    cancelButtonText: 'Cancelar'
-                }).then((result) => {
+                                    return;
+                                }
+                                if (data.error && data.msg) {
 
-                    if (result.isConfirmed) {
+                                    Swal.fire({
+                                        icon: 'error',
+                                        title: 'Ups...',
+                                        text: data.msg
+                                    })
 
-                        $.ajax({
+                                }
+                            })
 
-                            url: 'api/deleteCart.php',
-                            dataType: 'json',
-
-                            success: function (data) {
-
-                                Swal.fire({
-                                    icon: 'success',
-                                    title: 'Carrito vacio',
-                                    text: 'Se ha vaciado el carrito satisfactoriamente!',
-                                    confirmButtonText:
-                                            'Continuar',
-
-                                }).then((result) => {
-
-                                    if (result.isConfirmed) {
-                                        document.location.reload();
-                                    }
-                                })
-
-                                return;
-
-
-                            },
-
-                            error: function (error, msg) {
-                                Swal.fire({
-                                    icon: 'error',
-                                    title: 'Ups...',
-                                    text: 'No se ha podido procesar el vaciado del carrito',
-                                })
-
-                            },
-
-                        });
-
-                    }
-                })
-
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Ups...',
-                    text: 'El carrito está vacio!',
-                })
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Ups...',
+                        text: 'El carrito está vacio!',
+                    })
+                }
             }
 
+            function deleteCart() {
+
+
+                if (total !== '0 €') {
+
+                    Swal.fire({
+                        title: 'Estás seguro/a?',
+                        text: "Se borrarán todos los productos del carrito!",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Si, vaciar carrito!',
+                        cancelButtonText: 'Cancelar'
+                    }).then((result) => {
+
+                        if (result.isConfirmed) {
+
+                            $.ajax({
+
+                                url: 'api/deleteCart.php',
+                                dataType: 'json',
+
+                                success: function (data) {
+
+                                    Swal.fire({
+                                        icon: 'success',
+                                        title: 'Carrito vacio',
+                                        text: 'Se ha vaciado el carrito satisfactoriamente!',
+                                        confirmButtonText:
+                                                'Continuar',
+
+                                    }).then((result) => {
+
+                                        if (result.isConfirmed) {
+                                            document.location.reload();
+                                        }
+                                    })
+
+                                    return;
+
+
+                                },
+
+                                error: function (error, msg) {
+                                    Swal.fire({
+                                        icon: 'error',
+                                        title: 'Ups...',
+                                        text: 'No se ha podido procesar el vaciado del carrito',
+                                    })
+
+                                },
+
+                            });
+
+                        }
+                    })
+
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Ups...',
+                        text: 'El carrito está vacio!',
+                    })
+                }
 
 
 
-        }
-    </script>
-</div>
+
+            }
+
+            function deleteItem(id) {
+
+                 $.ajax({
+               
+                url: 'api/deleteItem.php',
+
+              data: {
+                       itemId: id
+
+                   },
+
+               
+                type: 'POST',
+
+               
+                dataType: 'json',
+
+              
+                success: function (data) {
+                    
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Borrado!',
+                        text: 'Se ha quitado un elemento del carrito!',
+                        confirmButtonText:
+                                'Continuar',
+
+                    }).then((result) => {
+
+                        if (result.isConfirmed) {
+                            document.location.reload();
+                        }
+                    })
+                   
+                    return
+
+
+                },
+
+                error: function (error, msg) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Ups...',
+                        text: 'No se ha podido borrar el elemento!',
+                    })
+
+                },
+
+            });
+
+            }
+
+        </script>
+    </body>
+</html>
+
+
+
